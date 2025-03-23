0a01fb30bccc07040de54157c8777c01994a7245
Youssef Mohamed <yfathy20920@gmail.com>
2025-03-23 23:38:19 +0200

    change data.json path

--- services/otpService.js
+++ services/otpService.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const dataFilePath = path.join(__dirname, '../data/data.json');
+const dataFilePath = path.join(__dirname, '../data.json');
 
 const loadData = () => {
     if (!fs.existsSync(dataFilePath)) return {};
